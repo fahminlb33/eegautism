5fa97c29732d50a7f144fbf5448c3a7e8d8afd91
fahminlb33 <fahmi@kodesiana.com>
2024-11-19 06:23:40 +0700

    feat: add argparse

--- scripts/bci_convert.py
+++ scripts/bci_convert.py
@@ -1,9 +1,39 @@
 import os
 import glob
+import argparse
+import dataclasses
 import subprocess
+from pprint import pprint
 
-files = glob.glob("data/bci2000/normal/*.*")
-for file in files:
-    output_name = os.path.splitext(os.path.basename(file))[0] + ".edf"
-    output_path = os.path.join("data/edf/normal", output_name)
-    subprocess.call(["bin2rec", "-f=EDF", file, output_path])
+
+@dataclasses.dataclass
+class CliArguments:
+    input_path: str
+    output_path: str
+
+    @staticmethod
+    def parse(args):
+        norm = {k.replace("-", "_"): v for k, v in vars(args).items()}
+        return CliArguments(**norm)
+
+
+def main(args: CliArguments):
+    for file in glob.glob(os.path.join(args.input_path, "*.dat")):
+        print("Processing: ", file)
+        output_name = os.path.splitext(os.path.basename(file))[0] + ".edf"
+        output_path = os.path.join(args.output_path, output_name)
+        subprocess.call(["bin2rec", "-f=EDF", file, output_path])
+
+
+if __name__ == "__main__":
+    # create CLI parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input-path", type=str)
+    parser.add_argument("output-path", type=str)
+
+    # parse CLI
+    args = CliArguments.parse(parser.parse_args())
+    pprint(repr(args))
+
+    # start the app!
+    main(args)
