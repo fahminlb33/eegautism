b4122cc1955a2bb47cb62aa5acb57e7671aa5ae0
fahminlb33 <fahmi@kodesiana.com>
2025-01-07 15:01:55 +0700

    feat: add executors

--- scripts/tune.sh
+++ scripts/tune.sh
@@ -2,31 +2,35 @@
 set -e
 
 # vanillacnn1d
-python scripts/tune.py './data/processed/cnn1d/data_10.nc' './results' --arch vanillacnn1d --test-size 0.30
-python scripts/tune.py './data/processed/cnn1d/data_10.nc' './results' --arch vanillacnn1d --test-patients "mahmud,bader,mohammed"
-python scripts/tune.py './data/processed/cnn1d/data_10_2.nc' './results' --arch vanillacnn1d --test-size 0.30
-python scripts/tune.py './data/processed/cnn1d/data_10_2.nc' './results' --arch vanillacnn1d --test-patients "mahmud,bader,mohammed"
+python scripts/tune.py './data/processed/cnn1d/data_10.nc' './results/tune-new.jsonl' --arch vanillacnn1d --test-size 0.30
+python scripts/tune.py './data/processed/cnn1d/data_10.nc' './results/tune-new.jsonl' --arch vanillacnn1d --test-patients "mahmud,bader,mohammed"
+python scripts/tune.py './data/processed/cnn1d/data_10_2.nc' './results/tune-new.jsonl' --arch vanillacnn1d --test-size 0.30
+python scripts/tune.py './data/processed/cnn1d/data_10_2.nc' './results/tune-new.jsonl' --arch vanillacnn1d --test-patients "mahmud,bader,mohammed"
 
 # eegnet
-python scripts/tune.py './data/processed/cnn1d/data_10.nc' './results' --arch eegnet --test-size 0.30
-python scripts/tune.py './data/processed/cnn1d/data_10.nc' './results' --arch eegnet --test-patients "mahmud,bader,mohammed"
-python scripts/tune.py './data/processed/cnn1d/data_10_2.nc' './results' --arch eegnet --test-size 0.30
-python scripts/tune.py './data/processed/cnn1d/data_10_2.nc' './results' --arch eegnet --test-patients "mahmud,bader,mohammed"
+python scripts/tune.py './data/processed/cnn1d/data_10.nc' './results/tune-new.jsonl' --arch eegnet --test-size 0.30
+python scripts/tune.py './data/processed/cnn1d/data_10.nc' './results/tune-new.jsonl' --arch eegnet --test-patients "mahmud,bader,mohammed"
+python scripts/tune.py './data/processed/cnn1d/data_10_2.nc' './results/tune-new.jsonl' --arch eegnet --test-size 0.30
+python scripts/tune.py './data/processed/cnn1d/data_10_2.nc' './results/tune-new.jsonl' --arch eegnet --test-patients "mahmud,bader,mohammed"
 
 # deepsleepnet
-python scripts/tune.py './data/processed/cnn1d/data_10.nc' './results' --arch deepsleepnet --test-size 0.30
-python scripts/tune.py './data/processed/cnn1d/data_10.nc' './results' --arch deepsleepnet --test-patients "mahmud,bader,mohammed"
-python scripts/tune.py './data/processed/cnn1d/data_10_2.nc' './results' --arch deepsleepnet --test-size 0.30
-python scripts/tune.py './data/processed/cnn1d/data_10_2.nc' './results' --arch deepsleepnet --test-patients "mahmud,bader,mohammed"
+python scripts/tune.py './data/processed/cnn1d/data_10.nc' './results/tune-new.jsonl' --arch deepsleepnet --test-size 0.30
+python scripts/tune.py './data/processed/cnn1d/data_10.nc' './results/tune-new.jsonl' --arch deepsleepnet --test-patients "mahmud,bader,mohammed"
+python scripts/tune.py './data/processed/cnn1d/data_10_2.nc' './results/tune-new.jsonl' --arch deepsleepnet --test-size 0.30
+python scripts/tune.py './data/processed/cnn1d/data_10_2.nc' './results/tune-new.jsonl' --arch deepsleepnet --test-patients "mahmud,bader,mohammed"
 
 # chrononet
-python scripts/tune.py './data/processed/cnn1d/data_10.nc' './results' --arch chrononet --test-size 0.30
-python scripts/tune.py './data/processed/cnn1d/data_10.nc' './results' --arch chrononet --test-patients "mahmud,bader,mohammed"
-python scripts/tune.py './data/processed/cnn1d/data_10_2.nc' './results' --arch chrononet --test-size 0.30
-python scripts/tune.py './data/processed/cnn1d/data_10_2.nc' './results' --arch chrononet --test-patients "mahmud,bader,mohammed"
+python scripts/tune.py './data/processed/cnn1d/data_10.nc' './results/tune-new.jsonl' --arch chrononet --test-size 0.30
+python scripts/tune.py './data/processed/cnn1d/data_10.nc' './results/tune-new.jsonl' --arch chrononet --test-patients "mahmud,bader,mohammed"
+python scripts/tune.py './data/processed/cnn1d/data_10_2.nc' './results/tune-new.jsonl' --arch chrononet --test-size 0.30
+python scripts/tune.py './data/processed/cnn1d/data_10_2.nc' './results/tune-new.jsonl' --arch chrononet --test-patients "mahmud,bader,mohammed"
 
 # filipnet
-python scripts/tune.py './data/processed/cnn1d/data_10.nc' './results' --arch filipnet --test-size 0.30
-python scripts/tune.py './data/processed/cnn1d/data_10.nc' './results' --arch filipnet --test-patients "mahmud,bader,mohammed"
-python scripts/tune.py './data/processed/cnn1d/data_10_2.nc' './results' --arch filipnet --test-size 0.30
-python scripts/tune.py './data/processed/cnn1d/data_10_2.nc' './results' --arch filipnet --test-patients "mahmud,bader,mohammed"
+python scripts/tune.py './data/processed/cnn1d/data_10.nc' './results/tune-new.jsonl' --arch filipnet --test-size 0.30
+python scripts/tune.py './data/processed/cnn1d/data_10.nc' './results/tune-new.jsonl' --arch filipnet --test-patients "mahmud,bader,mohammed"
+python scripts/tune.py './data/processed/cnn1d/data_10_2.nc' './results/tune-new.jsonl' --arch filipnet --test-size 0.30
+python scripts/tune.py './data/processed/cnn1d/data_10_2.nc' './results/tune-new.jsonl' --arch filipnet --test-patients "mahmud,bader,mohammed"
+
+sleep 5s
+
+shutdown.exe -s -t 0
